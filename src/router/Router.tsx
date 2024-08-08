927dfc46ce71726d276b0824304476d3b18be05c
lex banzon jr <lexbanzonjr@gmail.com>
2024-08-08 16:17:35 -0700

    setup routes

---
+++ src/router/Router.tsx
@@ -0,0 +1,8 @@
+import { useRoutes } from "react-router-dom";
+
+const Router = ({ allRoutes }: { allRoutes: any }) => {
+  const routes = useRoutes([...allRoutes]);
+  return routes;
+};
+
+export default Router;
