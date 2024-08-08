eab6cb6efa165f65f3fdde7500de5fc672567b04
lex banzon jr <lexbanzonjr@gmail.com>
2024-08-08 16:31:04 -0700

    Add public routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./index.css";
 import Router from "./router/Router";
+import publicRoutes from "./router/routes/publicRoutes";
 
 function App() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [allRoutes, setAllRoutes] = useState([]);
+  const [allRoutes, setAllRoutes] = useState([...publicRoutes]);
+  console.log(allRoutes);
   return <Router allRoutes={allRoutes} />;
 }
 
