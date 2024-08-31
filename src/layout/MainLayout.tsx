0701824cf37bbb732b43187cc8a0c128d94f1df9
lex banzon jr <lexbanzonjr@gmail.com>
2024-08-30 19:06:51 -0700

    sidebar and header

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 const MainLayout = () => {
   return (
-    <div>
-      <h1>
-        Main layout <Outlet />
-      </h1>
+    <div className="bg-[#cdcae9] w-full min-h-screen ">
+      <Header />
+      <Sidebar />
+      <div className="ml-0 lg:ml-[260] pt-[95px] transition-all">
+        <Outlet />
+      </div>
     </div>
   );
 };
--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Sidebar = () => {
   return (
     <div>
-      <h1>Sidebar</h1>
+      <div></div>
+      <div
+        className={`w-[260px] fixed bg-[#e6e7fb] z-50 top-0 h-screen shadow-[0_0_15px_0rgb(34_41_47_/5%)] transition-all`}
+      >
+        <div className="h-[70px] flex justify-center items-center">
+          <Link to="/" className="w-[180px] h-[50px]">
+            <img
+              className="w-full h-full"
+              src="/Creationz-8-9-2024.png"
+              alt=""
+            />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
