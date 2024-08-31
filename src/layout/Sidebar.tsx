db93ea829b8fdba2d164f79258a67639de45ac58
lex banzon jr <lexbanzonjr@gmail.com>
2024-08-30 21:01:50 -0700

    Add nav to sidebar

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { getNav, Nav } from "../navigation";
+import { BiLogOutCircle } from "react-icons/bi";
 
 const Sidebar = () => {
+  const { pathname } = useLocation();
   const [allNav, setAllNav] = useState<Nav[]>([]);
   useEffect(() => {
     const navs = getNav("admin");
     setAllNav(navs);
-    console.log(navs);
   }, []);
 
   return (
@@ -25,7 +26,33 @@ const Sidebar = () => {
             />
           </Link>
         </div>
-        <div className="px-[16px]"></div>
+        <div className="px-[16px]">
+          <ul>
+            {allNav.map((n, i) => (
+              <li key={1}>
+                <Link
+                  to={n.path}
+                  className={`${
+                    pathname === n.path
+                      ? "bg-blue-600 shadow-indigo-500/50 text-white duration-500"
+                      : "text-[#030811] font-bold duration-200"
+                  } px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-6 transition-all w-full mb-1`}
+                >
+                  <span>{n.icon}</span>
+                  <span>{n.title}</span>
+                </Link>
+              </li>
+            ))}
+            <li>
+              <button className="text-[#030811] font-bold duration-200 px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-6 transition-all w-full mb-1">
+                <span>
+                  <BiLogOutCircle />
+                </span>
+                <span>Logout</span>
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/navigation/allNav.tsx
+++ src/navigation/allNav.tsx
@@ -1,4 +1,9 @@
-import { AiOutlineDashboard } from "react-icons/ai";
+import { AiOutlineDashboard, AiOutlineShoppingCart } from "react-icons/ai";
+import { BiCategory } from "react-icons/bi";
+import { FaUsers, FaUserTimes } from "react-icons/fa";
+import { FaCodePullRequest } from "react-icons/fa6";
+import { IoIosChatbubbles } from "react-icons/io";
+import { MdPayment } from "react-icons/md";
 
 export const allNav = [
   {
@@ -7,5 +12,54 @@ export const allNav = [
     icon: <AiOutlineDashboard />,
     role: "admin",
     path: "/admin/dashboard",
+  },
+  {
+    id: 2,
+    title: "Orders",
+    icon: <AiOutlineShoppingCart />,
+    role: "admin",
+    path: "/admin/dashboard/orders",
+  },
+  {
+    id: 3,
+    title: "Category",
+    icon: <BiCategory />,
+    role: "admin",
+    path: "/admin/dashboard/category",
+  },
+  {
+    id: 4,
+    title: "Seller",
+    icon: <FaUsers />,
+    role: "admin",
+    path: "/admin/dashboard/seller",
+  },
+  {
+    id: 5,
+    title: "Payment Request",
+    icon: <MdPayment />,
+    role: "admin",
+    path: "/admin/dashboard/payment-request",
+  },
+  {
+    id: 6,
+    title: "Deactive Sellers",
+    icon: <FaUserTimes />,
+    role: "admin",
+    path: "/admin/dashboard/deactive-sellers",
+  },
+  {
+    id: 7,
+    title: "Seller Request",
+    icon: <FaCodePullRequest />,
+    role: "admin",
+    path: "/admin/dashboard/seller-request",
+  },
+  {
+    id: 8,
+    title: "Live Chat",
+    icon: <IoIosChatbubbles />,
+    role: "admin",
+    path: "/admin/dashboard/seller-request",
   },
 ];
