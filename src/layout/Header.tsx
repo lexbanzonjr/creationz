17e10997d92b25537fabb80f1d531efc25c57f9e
lex banzon jr <lexbanzonjr@gmail.com>
2024-09-06 13:00:16 -0700

    Add avatar

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -9,7 +9,7 @@ const Header = ({
 }) => {
   return (
     <div className="fixed top-0 left-0 w-full py-5 px-2 lg:px-7 z-40">
-      <div className="ml-0 lg:ml-[260px] rounded-md h-[65px] flex justify-between items-start bg-[#b1addf] px-5 transition-all">
+      <div className="ml-0 lg:ml-[260px] rounded-md h-[65px] flex justify-between items-center bg-[#b1addf] px-5 transition-all">
         <div
           onClick={() => setShowSidebar(!showSidebar)}
           className="w-[35px] flex lg:hidden h-[35px] rounded-sm bg-indigo-500 shadow-lg hover:shadow-indigo-500/50 justify-center items-center cursor-pointer"
@@ -17,6 +17,29 @@ const Header = ({
           <span>
             <FaList />
           </span>
+        </div>
+        <div className="hidden md:block">
+          <input
+            className="px-3 py-2 outline-none border bg-transparent border-slate-700 rounded-md text-[#cdcae9] focus:border-indigo-500 overflow-hidden"
+            type="text"
+            name="search"
+            placeholder="search"
+          />
+        </div>
+        <div className="flex justify-center items-center gap-8 relative">
+          <div className="flex justify-center items-center">
+            <div className="flex justify-center items-center gap-3">
+              <div className="flex justify-center items-center flex-col text-end">
+                <h2 className="text-md font-bold">Lex Banzon</h2>
+                <span className="text-[14] w-full font-normal">Admin</span>
+              </div>
+              <img
+                className="w-[45px] h-[45px] rounded-full overflow-hidden"
+                src="/blank-person.jpg"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
