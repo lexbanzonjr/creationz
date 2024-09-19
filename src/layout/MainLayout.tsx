0261cae87e876f9098de1d7c1e77bf14a1b8a065
lex banzon jr <lexbanzonjr@gmail.com>
2024-09-18 19:07:53 -0700

    Fixed styles

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -10,7 +10,7 @@ const MainLayout = () => {
     <div className="bg-[#cdcae9] w-full min-h-screen ">
       <Header showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
       <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
-      <div className="ml-0 lg:ml-[260] pt-[95px] transition-all">
+      <div className="ml-0 lg:ml-[260px] pt-[95px] transition-all">
         <Outlet />
       </div>
     </div>
--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -23,14 +23,14 @@ const Sidebar = ({
         onClick={() => {
           setShowSidebar(false);
         }}
-        className={`flex duration-200 ${
+        className={`fixed duration-200 ${
           !showSidebar ? "invisible" : "visible"
         } w-screen h-screen bg-[#22292f80] top-0 left-0 z-10`}
       ></div>
       <div
-        className={`w-[260px] fixed bg-[#e6e7fb] z-50 top-0 h-screen shadow-[0_0_15px_0rgb(34_41_47_/5%)] transition-all ${
+        className={`w-[260px] fixed bg-[#e6e7fb] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all ${
           showSidebar ? "left-0" : "-left-[260px] lg:left-0"
-        }`}
+        } `}
       >
         <div className="h-[70px] flex justify-center items-center">
           <Link to="/" className="w-[180px] h-[50px]">
@@ -50,8 +50,8 @@ const Sidebar = ({
                   className={`${
                     pathname === n.path
                       ? "bg-blue-600 shadow-indigo-500/50 text-white duration-500"
-                      : "text-[#030811] font-bold duration-200"
-                  } px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-6 transition-all w-full mb-1`}
+                      : "text-[#030811] font-bold duration-200 "
+                  } px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1 `}
                 >
                   <span>{n.icon}</span>
                   <span>{n.title}</span>
@@ -59,7 +59,7 @@ const Sidebar = ({
               </li>
             ))}
             <li>
-              <button className="text-[#030811] font-bold duration-200 px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-6 transition-all w-full mb-1">
+              <button className="text-[#030811] font-bold duration-200 px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1">
                 <span>
                   <BiLogOutCircle />
                 </span>
