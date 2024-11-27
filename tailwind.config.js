c9712e96d524b096ce745746ea04cab75a715857
lex banzon jr <lexbanzonjr@gmail.com>
2024-11-27 14:51:57 -0800

    Setup Tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Update this path based on your project structure
+    "./public/index.html",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
