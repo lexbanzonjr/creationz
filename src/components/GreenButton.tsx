c85514e9b4e8f0b5eb391fca35bb95be37df754e
lex banzon jr <lexbanzonjr@gmail.com>
2024-12-21 11:29:41 -0800

    Add disabled property

--- src/components/GreenButton.tsx
+++ src/components/GreenButton.tsx
@@ -3,14 +3,22 @@ import { ReactNode } from "react";
 const GreenButton: React.FC<{
   children: ReactNode;
   className?: string;
+  disabled?: boolean;
   type?: "button" | "submit" | "reset";
   onClick?: () => void;
-}> = ({ children, className = "", type = "button", onClick }) => {
+}> = ({
+  children,
+  className = "",
+  disabled = false,
+  type = "button",
+  onClick,
+}) => {
   return (
     <button
+      disabled={disabled}
       type={type}
       onClick={onClick}
-      className={`block px-1 py-1 bg-green-500 text-white text-base cursor-pointer hover:bg-green-600 ${className}`}
+      className={`block px-1 py-1 bg-green-500 text-white text-base cursor-pointer hover:bg-green-600 disabled:bg-gray-400 disabled:cursor-not-allowed ${className}`}
     >
       {children}
     </button>
