4c616eddecc7149db006da2397d71912fafcc887
lex banzon jr <lexbanzonjr@gmail.com>
2025-01-16 22:05:35 -0800

    Check-in useAdminStore.ts

--- src/hooks/useAdminStore.ts
+++ src/hooks/useAdminStore.ts
@@ -1,14 +1,47 @@
 import { create } from "zustand";
-import { Category, Product, Type } from "../types/global";
+import { Category, Option, Product, Type } from "../types/global";
 import {
   addCategory as addCategoryApi,
   deleteCategory as deleteCategoryApi,
+  updateCategory as updateCategoryApi,
 } from "../api/categoryApi";
 import {
   addProduct as addProductApi,
   deleteProduct as deleteProductApi,
 } from "../api/productApi";
-import { deleteType as deleteTypeApi } from "../api/typeApi";
+import {
+  addType as addTypeApi,
+  addTypeOption as addTypeOptionApi,
+  deleteType as deleteTypeApi,
+} from "../api/typeApi";
+
+const blankCategory: Category = {
+  _id: "",
+  name: "",
+  description: "",
+  designs: [],
+};
+
+const blankOption: Option = {
+  _id: "",
+  name: "",
+};
+
+const blankType: Type = {
+  _id: "",
+  name: "",
+  options: [],
+};
+
+const getNext = <T>(current: T, list: T[], blankObject: T) => {
+  const index = list.findIndex((value) => current === value);
+  if (index + 1 < list.length) {
+    return list[index + 1];
+  } else if (list.length > 1) {
+    return list[index - 1];
+  }
+  return blankObject;
+};
 
 interface DataState {
   categories: Category[];
@@ -16,16 +49,30 @@ interface DataState {
   types: Type[];
   populate: boolean;
 
-  activeCategory?: Category;
-  setActiveCategory: (activeCategory: Category) => void;
+  activeCategory: Category;
+  activeOption: Option;
+  activeType: Type;
 
-  addCategory: (accessToken: string, category: Category) => Promise<Category>;
+  addCategory: (accessToken: string, category?: Category) => Promise<Category>;
   addProduct: (accessToken: string, product: Product) => Promise<Product>;
+  addType: (accessToken: string, type: Type) => Promise<Type>;
+  addTypeOption: (
+    accessToken: string,
+    type: Type,
+    option: Option
+  ) => Promise<Option>;
 
   deleteCategory: (accessToken: string, category: Category) => Promise<void>;
   deleteProduct: (accessToken: string, product: Product) => Promise<void>;
   deleteType: (accessToken: string, type: Type) => Promise<void>;
 
+  resetActiveCategory: () => void;
+  resetActiveType: () => void;
+  resetActiveOption: () => void;
+
+  setActiveCategory: (activeCategory: Category) => Category;
+  setActiveOption: (activeOption: Option) => Option;
+  setActiveType: (activeType: Type) => Type;
   setCategories: (categories: Category[]) => void;
   setProducts: (products: Product[]) => void;
   setTypes: (types: Type[]) => void;
@@ -41,6 +88,11 @@ interface DataState {
     types: Type[];
     populate: boolean;
   }) => void;
+
+  updateCategory: (
+    accessToken: string,
+    category: Category
+  ) => Promise<Category>;
 }
 
 const useStore = create<DataState>((set) => ({
@@ -49,13 +101,37 @@ const useStore = create<DataState>((set) => ({
   types: [],
   populate: false,
 
-  activeCategory: undefined,
+  activeCategory: blankCategory,
+  resetActiveCategory: () => {
+    set({ activeCategory: blankCategory });
+  },
   setActiveCategory: (activeCategory) => {
     set({ activeCategory });
+    return activeCategory;
+  },
+
+  activeType: blankType,
+  resetActiveType: () => {
+    set({ activeType: blankType });
+  },
+  setActiveType: (activeType) => {
+    set({ activeType });
+    return activeType;
+  },
+
+  activeOption: blankOption,
+  resetActiveOption: () => set({ activeOption: blankOption }),
+  setActiveOption: (option) => {
+    set({ activeOption: option });
+    return option;
   },
 
   addCategory: async (accessToken, category) => {
-    const newCategory = await addCategoryApi({ accessToken, category });
+    let newCategory = blankCategory;
+    if (category) {
+      newCategory = { ...newCategory, ...category };
+    }
+    newCategory = await addCategoryApi({ accessToken, category: newCategory });
     set((state) => {
       return { categories: [...state.categories, newCategory] };
     });
@@ -66,11 +142,30 @@ const useStore = create<DataState>((set) => ({
     set((state) => ({ products: [...state.products, newProduct] }));
     return newProduct;
   },
+  addType: async (accessToken, type) => {
+    const newType = await addTypeApi({ accessToken, type });
+    set((state) => ({ types: [...state.types, newType] }));
+    return newType;
+  },
+  addTypeOption: async (accessToken: string, type: Type, option: Option) => {
+    const newOption = await addTypeOptionApi(accessToken, type, option);
+    set((state) => ({
+      activeType: {
+        ...state.activeType,
+        options: [...state.activeType.options, newOption],
+      },
+    }));
+    return newOption;
+  },
 
   deleteCategory: async (accessToken, category) => {
     await deleteCategoryApi({ accessToken, category });
     set((state) => ({
       categories: state.categories.filter((item) => item._id !== category._id),
+      activeCategory:
+        category._id === state.activeCategory._id
+          ? getNext(category, state.categories, blankCategory)
+          : state.activeCategory,
     }));
   },
   deleteProduct: async (accessToken, product) => {
@@ -83,10 +178,21 @@ const useStore = create<DataState>((set) => ({
     await deleteTypeApi({ accessToken, type });
     set((state) => ({
       types: state.types.filter((item) => item._id !== type._id),
+      activeType:
+        type === state.activeType
+          ? getNext(type, state.types, blankType)
+          : state.activeType,
     }));
   },
 
-  setCategories: (categories) => set({ categories }),
+  setCategories: (categories) => {
+    set(() => ({
+      categories: categories.map((category) => ({
+        ...blankCategory,
+        category,
+      })),
+    }));
+  },
   setProducts: (products) => set({ products }),
   setTypes: (types) => set({ types }),
   setPopulate: (populate) => set({ populate }),
@@ -102,6 +208,16 @@ const useStore = create<DataState>((set) => ({
     types: Type[];
     populate: boolean;
   }) => set({ categories, products, types, populate }),
+
+  updateCategory: async (accessToken: string, category: Category) => {
+    const updateCategory = await updateCategoryApi({ accessToken, category });
+    set((state) => ({
+      categories: state.categories.map((cat) =>
+        cat._id === category._id ? { ...cat, ...category } : cat
+      ),
+    }));
+    return updateCategory;
+  },
 }));
 
 export default useStore;
